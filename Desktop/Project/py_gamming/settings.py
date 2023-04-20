a0a94ed557442eceb511af41f471dab0193a1b8f
Roger Allan Correia Figueira <rogerallan4004@gmail.com>
2023-04-19 22:14:37 -0300

    limit in edges,ship_speed making ships move.

---
+++ Desktop/Project/py_gamming/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alien Invasion """
+    def __init__(self):
+        # screen settigs.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (222, 232, 255)
+        # ship settings
+        self.ship_speed = 2.5
